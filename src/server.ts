16e8b7accc9b83cfd5bffa662496d665b2246335
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-12 23:47:33 +0900

    セッション認証サーバー作成

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
 const express = require("express");
 
 const session = require("express-session");
-const RedisStore = require('connect-redis');
-const { createClient } = require("redis");
+import {RedisStore} from "connect-redis"
+import {createClient} from "redis"
 
 import authRouter from "./routes/auth";
 
 const redisClient = createClient({
     url: process.env.REDIS_URL,
-});
+})
 redisClient.connect().catch(console.error);
 
+// Initialize store.
 const redisStore = new RedisStore({
     client: redisClient,
-    prefix: "prefix:",
-  });
+    prefix: "myapp:",
+})
 
 const dotenv = require("dotenv");
 const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env";
@@ -25,17 +26,22 @@ const app = express();
 // セッションを開始したときのクッキーへの送信とRedisへの登録の設定
 app.use(
   session({
+    store: redisStore,
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: Number(process.env.SESSION_COOKIE_MAX_AGE) },
-    store: redisStore,
+    saveUninitialized: false,
+    cookie: { 
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30日
+        httpOnly: true, // クッキーを通じてのJavaScriptアクセスを禁止
+        secure: process.env.NODE_ENV === "production", // 本番環境ではHTTPS接続が必要
+        sameSite: "lax", // クロスサイトリクエストを許可
+    },
   })
 );
 
 // ルーティング
 app.use("/auth", authRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4400, () => {
+  console.log("Server is running on port 4400");
 });
